7e8493ea61597c8355dce1394b7c1d6e660f3915
akhmadjonjurayev <formyproject1999@gmail.com>
2021-04-17 15:59:25 +0500

    draw

---
+++ Canculyator/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace Canculyator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.X2 = new System.Windows.Forms.Label();
+            this.X1 = new System.Windows.Forms.Label();
+            this.deter = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.mk4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.mk3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mk2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mk1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.X2);
+            this.panel1.Controls.Add(this.X1);
+            this.panel1.Controls.Add(this.deter);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.mk4);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.mk3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.mk2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.mk1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(219, 432);
+            this.panel1.TabIndex = 0;
+            // 
+            // X2
+            // 
+            this.X2.AutoSize = true;
+            this.X2.Location = new System.Drawing.Point(91, 357);
+            this.X2.Name = "X2";
+            this.X2.Size = new System.Drawing.Size(0, 15);
+            this.X2.TabIndex = 19;
+            // 
+            // X1
+            // 
+            this.X1.AutoSize = true;
+            this.X1.Location = new System.Drawing.Point(91, 307);
+            this.X1.Name = "X1";
+            this.X1.Size = new System.Drawing.Size(0, 15);
+            this.X1.TabIndex = 18;
+            // 
+            // deter
+            // 
+            this.deter.AutoSize = true;
+            this.deter.ForeColor = System.Drawing.Color.Red;
+            this.deter.Location = new System.Drawing.Point(10, 392);
+            this.deter.Name = "deter";
+            this.deter.Size = new System.Drawing.Size(0, 15);
+            this.deter.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(31, 358);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(20, 15);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "X2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 307);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 15);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "X1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(10, 270);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 15);
+            this.label6.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mk4
+            // 
+            this.mk4.Location = new System.Drawing.Point(91, 194);
+            this.mk4.Multiline = true;
+            this.mk4.Name = "mk4";
+            this.mk4.Size = new System.Drawing.Size(75, 23);
+            this.mk4.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "k";
+            // 
+            // mk3
+            // 
+            this.mk3.Location = new System.Drawing.Point(91, 148);
+            this.mk3.Multiline = true;
+            this.mk3.Name = "mk3";
+            this.mk3.Size = new System.Drawing.Size(75, 23);
+            this.mk3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "C";
+            // 
+            // mk2
+            // 
+            this.mk2.Location = new System.Drawing.Point(91, 103);
+            this.mk2.Multiline = true;
+            this.mk2.Name = "mk2";
+            this.mk2.Size = new System.Drawing.Size(75, 23);
+            this.mk2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "B";
+            // 
+            // mk1
+            // 
+            this.mk1.Location = new System.Drawing.Point(91, 57);
+            this.mk1.Multiline = true;
+            this.mk1.Name = "mk1";
+            this.mk1.Size = new System.Drawing.Size(75, 23);
+            this.mk1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "A*X^2+B*X+C";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(91, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 433);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox mk4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox mk3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox mk2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox mk1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label deter;
+        private System.Windows.Forms.Label X2;
+        private System.Windows.Forms.Label X1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Canculyator/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Canculyator
+{
+    public partial class Form1 : Form
+    {
+        private float Markaz_X = 482.0f;
+        private float Markaz_Y = 216.0f;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(Check(mk1.Text,mk2.Text,mk3.Text,mk4.Text))
+            {
+                float A, B, C, Determinant;
+                float k;
+                A = Convert.ToSingle(mk1.Text);
+                B = Convert.ToSingle(mk2.Text);
+                C = Convert.ToSingle(mk3.Text);
+                k = Convert.ToSingle(mk4.Text);
+                Determinant = B * B - 4 * A * C;
+                if(Determinant < 0)
+                {
+                    deter.Text = "berilgan funksiya ildizlarga ega emas";
+                }
+                else
+                {
+                    double x1, x2;
+                    x1 = (-B + Math.Sqrt(Determinant)) / (2 * A);
+                    x2 = (-B - Math.Sqrt(Determinant)) / (2 * A);
+                    X1.Text = x1.ToString();
+                    X2.Text = x2.ToString();
+                }
+                DrawFunction(A, B, C, k);
+            }
+            else
+            {
+                deter.Text = "input qiymatlaridan biri null qiymatga ega";
+            }
+        }
+        private bool Check(params string[] Text)
+        {
+            foreach (var text in Text)
+                if (text == string.Empty)
+                    return false;
+            return true;
+        }
+        private void DrawFunction(float A,float B,float C,float k)
+        {
+            Pen pen = new Pen(Color.Black, 0.1f);
+            Graphics graphics = this.CreateGraphics();
+            for (float i = -200f; i < 200f; i +=2 * k)
+            {
+                DrawLine(i, A * i * i + B * i + C, i + k, A * (i + k) * (i + k) + B * (i + k) + C, k);
+            }
+
+        }
+        private void DrawLine(float x1 ,float y1,float x2,float y2,float k)
+        {
+            Pen pen = new Pen(Color.Black, 0.1f);
+            Graphics graphics = this.CreateGraphics();
+            graphics.DrawLine(pen, x1 + Markaz_X,(-1) * y1 + Markaz_Y, x2 + Markaz_X, (-1) * y2 + Markaz_Y);
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Black, 0.1f);
+            e.Graphics.DrawLine(pen, Markaz_X, 0, Markaz_X, 2 * Markaz_Y);
+            e.Graphics.DrawLine(pen, 0, Markaz_Y, 2 * Markaz_X, Markaz_Y);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = this.CreateGraphics();
+            graphics.Clear(Color.White);
+            deter.Text = "";
+            X1.Text = "";
+            X2.Text = "";
+            Pen pen = new Pen(Color.Black, 0.1f);
+            graphics.DrawLine(pen, Markaz_X, 0, Markaz_X, 2 * Markaz_Y);
+            graphics.DrawLine(pen, 0, Markaz_Y, 2 * Markaz_X, Markaz_Y);
+        }
+    }
+}
